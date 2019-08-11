ab40aa99cdd6107dee34f09282b88d8d88834bca
Abhishek U H <abhishekuh94@gmail.com>
2019-08-12 02:05:18 +0530

    Updates

---
+++ precache-manifest.aa386ff7f08d5b2a3861717e06e5d7d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f511765361064025cc5ad3cf8e38b3a6",
+    "url": "/abhishekuh/index.html"
+  },
+  {
+    "revision": "17b0434b81a53928893d",
+    "url": "/abhishekuh/static/css/main.de472235.chunk.css"
+  },
+  {
+    "revision": "a7920afca8ccd5ca8cf0",
+    "url": "/abhishekuh/static/js/2.f1422c00.chunk.js"
+  },
+  {
+    "revision": "17b0434b81a53928893d",
+    "url": "/abhishekuh/static/js/main.74be60cb.chunk.js"
+  },
+  {
+    "revision": "026416a89919fefe731d",
+    "url": "/abhishekuh/static/js/runtime~main.da2195f6.js"
+  }
+]);
