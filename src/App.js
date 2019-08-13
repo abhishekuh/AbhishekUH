d8ea26c85a8b15662210c14e3ca7e0a8cf1340ef
Abhishek U H <abhishekuh94@gmail.com>
2019-08-14 01:05:47 +0530

    gh pages setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, HashRouter , Switch , Link } from 'react-router-dom'
 import contact from './components/contact'
 import home from './components/home'
 import projects from './components/projects';
 
 function App() {
   return (
+    <HashRouter basename="/">
       <div>
      <nav className="navbar navbar-expand-lg navbar-light bg-light nav-width">
                 {/* <a className="navbar-brand" href="#">Abhishek</a> */}
@@ -15,28 +16,37 @@ function App() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+                    <ul className="navbar-nav nav-tabs">
+                        <li className="nav-item active" >
+                            <a className="nav-link"><Link to="/home">Home</Link></a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/projects">Projects</a>
+                        <li className="nav-item"  >
+                            <a className="nav-link" ><Link to="/projects">Projects</Link></a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact</a>
+                        <li className="nav-item" >
+                            <a className="nav-link"><Link to="/contact">Contact</Link></a>
                         </li>
                     </ul>
                 </div>
             </nav>
-      <Router>
-           <Switch>
+        {/* <div className="tab-content">
+            <div className="tab-pane fade show active" id="home" role="tabpanel" >
+                <home /> 
+            </div>
+            <div className="tab-pane fade" id="projects" role="tabpanel" >
+                projects
+            </div>
+            <div className="tab-pane fade" id="contact" role="tabpanel" >
+                contact 
+            </div>
+        </div> */}    
             <Route exact path="/" component={home}></Route>
             <Route path="/contact" component={contact}></Route>
             <Route path="/home" component={home}></Route>
             <Route path="/projects" component={projects}></Route>
-        </Switch>
-      </Router>   
-    </div>
+            </div>
+      </HashRouter>   
+    
   );
 }
 
